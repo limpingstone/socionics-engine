113089eede55e274b10e4a995e4c065093a8d9e2
Steven Chen Hao Nyeo <limpingstone@Lstone529-PCA.student.cwru.edu>
2019-01-01 15:00:02 -0700

    run_test.py: script to test the cognitive functions

---
+++ run_test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#
+# run_test.py - By Steven Chen Hao Nyeo 
+# The testing script for the cognitive function
+# Created: January 1, 2019
+
+from cognitive_function import *
+from entity import Entity
+
+e = Entity(Te, Ni, Se, Fi, Ti, Ne, Si, Fe)
