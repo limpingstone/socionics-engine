aff0433b2d414bc7a0f3199830028296befc981d
Steven Chen Hao Nyeo <limpingstone@Lstone529-PCA.student.cwru.edu>
2019-01-01 14:06:21 -0700

    defined the eight cognitive functions

---
+++ CognitiveFunction.py
@@ -0,0 +1,14 @@
+class CognitiveFunction: 
+	def __init__ (self, label, description)
+		self.label = label
+		self.description = description
+		
+Ni = CognitiveFunction("Introveted Intuition", "")	
+Ne = CognitiveFunction("Extroverted Intuition", "")
+Ti = CognitiveFunction("Introveted Thinking", "")
+Te = CognitiveFunction("Extroverted Thinking", "")
+Si = CognitiveFunction("Introveted Sensing", "")
+Se = CognitiveFunction("Extroverted Sensing", "")
+Fi = CognitiveFunction("Introveted Ethics", "")
+Fe = CognitiveFunction("Extroverted Ethics", "")
+
