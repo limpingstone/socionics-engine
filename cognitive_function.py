4059c4f8a7351c39ea4be0e8e63165bac03673c6
Steven Chen Hao Nyeo <limpingstone@Lstone529-PCA.student.cwru.edu>
2019-01-01 20:47:34 -0700

    added comments

--- cognitive_function.py
+++ cognitive_function.py
@@ -5,16 +5,24 @@
 # Created: January 1, 2019
 
 class CognitiveFunction: 
-	def __init__ (self, label, description):
+	def __init__ (self, label, name, description):
 		self.label = label
+		self.name = name
 		self.description = description
 		
-Ni = CognitiveFunction("Introveted Intuition", "")	
-Ne = CognitiveFunction("Extroverted Intuition", "")
-Ti = CognitiveFunction("Introveted Thinking", "")
-Te = CognitiveFunction("Extroverted Thinking", "")
-Si = CognitiveFunction("Introveted Sensing", "")
-Se = CognitiveFunction("Extroverted Sensing", "")
-Fi = CognitiveFunction("Introveted Ethics", "")
-Fe = CognitiveFunction("Extroverted Ethics", "")
+Ni = CognitiveFunction("Ni", "Introveted Intuition", "development over time, historicity, cause and effect, consequences, repetition, archetypal themes and examples, looking for causes in history or the past, past-future forecasting of event dynamics, rhythm, delay or act-now, past-turned imagination ")	
+
+Ne = CognitiveFunction("Ne", "Extroverted Intuition", "potential, permutation, isomorphism, semblance, essence, uncertainty, the unknown, opening up new \"windows\" and bringing up new possibilities in conversation, seeing opportunities, chance, being the first, refreshing informational suddenness, diversity of interests and involvements")
+
+Ti = CognitiveFunction("Ti", "Introveted Thinking", "structure, analysis, coherence, consistency, cogency, accordance, match, commensurability, understanding, order, or the lack of thereof")
+
+Te = CognitiveFunction("Te", "Extroverted Thinking", "efficiency, method, mechanism, knowledge, work, reason in motion, direction of activity into its most logical course of action, \"logic of actions\", utilitarianism, expediency, benefit ")
+
+Si = CognitiveFunction("Si", "Introveted Sensing", "homeostasis, continuity, smoothness, flow, satisfaction, aesthetics, quality of life, pleasure, relaxation, convenience, quality ")
+
+Se = CognitiveFunction("Se", "Extroverted Sensing", "sensing of immediate static qualities of objects, sensing of immediate reality, external appearance, texture, form, static objects, impact, direct physical effect, span, extent, scope ")
+
+Fi = CognitiveFunction("Fi", "Introveted Ethics", "internal harmony, resonance or dissonance of personal sentiments, sympathy, pity, compassion, support, condemnation, judgement, positive and negative emotional space ")
+
+Fe = CognitiveFunction("Fe", "Extroverted Ethics", "emotional atmosphere, romanticism, cooperation, treatment, qualitative judgement of behavior, sympathy, ethical estimations of observable actions, \"ethics of actions\" ")
 
