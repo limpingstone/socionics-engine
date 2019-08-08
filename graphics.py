8a34ffc9e406ea2c2e3039d9ce841273c8bc4a0f
Steven Chen Hao Nyeo <limpingstone@Lstone529-PCA.student.cwru.edu>
2019-08-08 11:24:12 +0800

    graphics.py - added comments and tweaks

--- graphics.py
+++ graphics.py
@@ -18,18 +18,22 @@ class TypeFrame(wx.Frame):
         
         # Create Frame
         wx.Frame.__init__(self, parent, title = title, size = (530, 480), style = wx.DEFAULT_FRAME_STYLE ^ wx.RESIZE_BORDER)
-
         self.panel = wx.Panel(self)
-       
+      
+        # The current list of cognitive functions entered into the system 
         self.entityList = []
+
+        # Arrays containing the rows of buttons for dominant and auxiliary functions
         self.domButtons = []
         self.auxButtons = []
+
+        # Keep track of the current row of buttons to enable
         self.rowCount = 0
 
+        # Setup for program interface
         self.row_1_y = 30
         self.row_2_y = 90
         self.row_3_y = 150
-
         wx.StaticText(self.panel, label = "Dominant Function:", pos = (30, self.row_1_y - 20))
         self.createCogButtons(0)
         wx.StaticText(self.panel, label = "Auxiliary Function:", pos = (30, self.row_2_y - 20))
@@ -37,29 +41,44 @@ class TypeFrame(wx.Frame):
 
     # The function that creates the buttons for the eight cognitive functions
     def createCogButtons(self, row):
+
+        # Keeps track of creation of dominant or auxiliary buttons
         cogButtons = self.domButtons if row == 0 else self.auxButtons 
+        
+        # Create and bind the buttons to the event
         labels = ["N", "S", "T", "F"]
         for i in range(4):   
             cogButtons.append(wx.Button(self.panel, label = labels[i] + "i", size = (50, 30) , pos = (30 + 120 * i, self.row_1_y if row == 0 else self.row_2_y)))
             cogButtons.append(wx.Button(self.panel, label = labels[i] + "e", size = (50, 30) , pos = (90 + 120 * i, self.row_1_y if row == 0 else self.row_2_y)))
         for i in range(8):
-            self.Bind(wx.EVT_BUTTON, self.onclick_cogFunc, cogButtons[i])
-        if (row == 1):
+            self.Bind(wx.EVT_BUTTON, self.onclick_cogFunction, cogButtons[i])
+
+        # The auxiliary buttons are disabled before the dominant function is entered
+        if (row == 1): 
             for button in self.auxButtons:
                 button.Disable()
 
-    def onclick_cogFunc(self, event):
+    # The event handler for clicking on the buttons
+    def onclick_cogFunction(self, event):
         btnLabel = event.GetEventObject().GetLabel()
+
+        # First row - dominant function
         if (self.rowCount == 0):
+
+            # Disable the dominant function buttons
             self.rowCount = 1
             self.entityList.append(self.labelToFunction(btnLabel))
             for button in self.domButtons:
                 button.Disable()
+
+            # Re-enable the appropriate auxiliary function buttons
             for button in self.auxButtons:
                 if (button.Label[1] == self.entityList[0].opposite().sublabel 
                         and button.Label[0] != self.entityList[0].opposite_orientation().label
                         and button.Label[0] != self.entityList[0].label):
                     button.Enable()
+
+        # Second row - auxiliary function
         else:
             self.entityList.append(self.labelToFunction(btnLabel))
             for button in self.auxButtons:
@@ -73,6 +92,7 @@ class TypeFrame(wx.Frame):
                         Translator.translate_decision_making(e) +
                         Translator.translate_perception(e))
 
+    # The helper functin that returns the corresponding function object according to the entered string
     def labelToFunction(self, btnLabel):
         if (btnLabel == "Ni"):                   
             return Ni
