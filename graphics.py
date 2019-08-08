ca5a853663284d08ff004d3aa32b66116840b951
Steven Chen Hao Nyeo <limpingstone@Lstone529-PCA.student.cwru.edu>
2019-08-08 11:27:59 +0800

    graphics.py: removed extra imports

--- graphics.py
+++ graphics.py
@@ -7,9 +7,6 @@
 import wx
 from cognitive_function import *
 from entity import Entity
-from value import Value
-from strength import Strength
-from consciousness import Consciousness
 from function_to_type import Translator
 from function_analysis import *
 
